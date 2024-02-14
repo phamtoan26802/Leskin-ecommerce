308a9962a7241c4a0d226f84bfa73e9f023a5976
phamtoan26802 <emcodi123@gmail.com>
2024-02-14 11:52:20 +0700

    project

--- Product/src/main/java/com/ecommerce/product/service/ProductService.java
+++ Product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -2,14 +2,16 @@ package com.ecommerce.product.service;
 
 import com.ecommerce.product.dto.ProductDto;
 import com.ecommerce.product.model.Product;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProductService {
     List<ProductDto> findAll();
-    Product save(ProductDto productDto);
-    Product update(ProductDto productDto);
+    Product save(MultipartFile imageProduct, ProductDto productDto);
+    Product update(MultipartFile imageProduct,ProductDto productDto);
     void deleteById(Long id);
     void enableById(Long id);
+    ProductDto getById(Long id);
 
 }
--- Product/src/main/java/com/ecommerce/product/service/imp/ProductServiceImpl.java
+++ Product/src/main/java/com/ecommerce/product/service/imp/ProductServiceImpl.java
@@ -4,15 +4,19 @@ import com.ecommerce.product.dto.ProductDto;
 import com.ecommerce.product.model.Product;
 import com.ecommerce.product.repository.ProductRepository;
 import com.ecommerce.product.service.ProductService;
+import com.ecommerce.product.utils.ImageUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ImageUpload imageUpload;
     @Override
     public List<ProductDto> findAll() {
         List<ProductDto> productDtoList = new ArrayList<>();
@@ -29,19 +33,81 @@ public class ProductServiceImpl implements ProductService {
             productDto.setImage(product.getImage());
             productDto.setActivated(product.is_activated());
             productDto.setDeleted(product.is_deleted());
-
+            productDtoList.add(productDto);
         }
         return productDtoList;
     }
 
     @Override
-    public Product save(ProductDto productDto) {
+    public Product save(MultipartFile imageProduct, ProductDto productDto){
+        try {
+            Product product = new Product();
+            if (imageProduct == null){
+                product.setImage(null);
+            }else {
+                if (imageUpload.uploadImage(imageProduct)){
+                    System.out.println("Upload successfully!");
+                }
+                product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
+            }
+            product.setName(productDto.getName());
+            product.setDescription(productDto.getDescription());
+            product.setCategory(productDto.getCategory());
+            product.setCostPrice(productDto.getCostPrice());
+            product.setCurrentQuantity(productDto.getCurrentQuantity());
+            product.set_activated(true);
+            product.set_deleted(false);
+            return productRepository.save(product);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @Override
+    public Product update(MultipartFile imageProduct, ProductDto productDto) {
+        try {
+            Product product = productRepository.getById(productDto.getId());
+            if (imageProduct == null){
+                product.setImage(product.getImage());
+            }else{
+                if(imageUpload.checkExisted(imageProduct) == false){
+                    System.out.println("Upload to folder");
+                    //imageUpload.uploadImage(imageProduct);
+                }
+                System.out.println("Image existed");
+                product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
+            }
+            product.setName(productDto.getName());
+            product.setDescription(productDto.getDescription());
+            product.setSalePrice(productDto.getSalePrice());
+            product.setCostPrice(productDto.getCostPrice());
+            product.setCurrentQuantity(productDto.getCurrentQuantity());
+            product.setCategory(productDto.getCategory());
+            return product;
+            //return productRepository.save(product);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public Product update(ProductDto productDto) {
-        return null;
+    public ProductDto getById(Long id) {
+        Product product = productRepository.getById(id);
+        ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName(product.getName());
+        productDto.setDescription(product.getDescription());
+        productDto.setCurrentQuantity(product.getCurrentQuantity());
+        productDto.setCategory(product.getCategory());
+        productDto.setSalePrice(product.getSalePrice());
+        productDto.setCostPrice(product.getCostPrice());
+        productDto.setImage(product.getImage());
+        productDto.setDeleted(product.is_deleted());
+        productDto.setActivated(product.is_activated());
+        return productDto;
     }
 
     @Override
